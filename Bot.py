189183b4e46f8c21ae8a53968e8b12cf6824420e
FireHeart786 <124797061+FireHeart786@users.noreply.github.com>
2023-02-08 19:28:50 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,34 @@
+from twilio.rest import Client
+
+# Your Twilio Account SID and Auth Token
+
+account_sid = 'Your Twilio Account SID'
+
+auth_token = 'Your Twilio Auth Token'
+
+client = Client(account_sid, auth_token)
+
+# The phone number you wish to send and receive texts from
+
+from_whatsapp_number = 'whatsapp:+14155238886'
+
+to_whatsapp_number = 'whatsapp:+12345678901'
+
+# The message you want to send as an autoreply
+
+autoreply_message = 'Thank you for your message. I am currently unavailable. I will get back to you soon.'
+
+# Send an autoreply message
+
+message = client.messages.create(
+
+    to=to_whatsapp_number,
+
+    from_=from_whatsapp_number,
+
+    body=autoreply_messag
+
+)
+
+print(message.sid)
+
